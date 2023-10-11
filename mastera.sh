e85402a8f43bd38d1b01d647266f36d706691df9
root <root@k8smater.example.net>
2023-10-11 22:36:29 +0000

    Se terminaron los archivos del máster

--- mastera.sh
+++ mastera.sh
@@ -36,10 +36,12 @@ EOF
 sudo sysctl --system
 
 sudo apt update
+sudo apt upgrade
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
+sudo apt upgrade
 sudo apt install -y containerd.io
 
 containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
@@ -51,7 +53,22 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearm
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 sudo apt update
+sudo apt upgrade
 
 sudo apt install -y kubelet kubeadm kubectl
 
 sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "Inicializando el nodo maestro... Aquí está el token"
+sudo kubeadm init --control-plane-endpoint=k8smaster.example.net
+
+echo "Configurando el cliente Kubernetes..."
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl cluster-info
+
+kubectl get nodes
+
+echo "Acciones completadas."
